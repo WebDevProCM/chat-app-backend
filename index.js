574a8c90f76c81c79dc440f5104263c8b812db04
Chamod <webdevprocm@gmail.com>
2024-09-01 17:06:03 +0530

    initial

---
+++ index.js
@@ -0,0 +1,78 @@
+const http = require("http");
+// const badWords = require("bad-words");
+const path =  require("path");
+const express = require("express");
+const socketio = require("socket.io");
+const cors = require("cors");
+const getTools = require("./utilis/getTools");
+
+const app = express();
+const server = new http.createServer(app);
+
+const io = socketio(server, {
+    cors: {
+        origin: "http://localhost:5173",
+    }
+});
+
+app.use(cors());
+app.use(express.json());
+
+const publicDirectory = path.join(__dirname, "./public");
+app.use(express.static(publicDirectory)); 
+
+io.on("connect", (socket) =>{
+    socket.on("room", (roomData, callback) =>{
+        const user = getTools.addUser(roomData.name, roomData.room, socket.id);
+        if(user.error){
+            return callback(user.error);
+        }
+
+        socket.join(user.room);
+        socket.emit("message", getTools.generateMessage(`Welcome ${user.username}!`, "System"));
+        socket.broadcast.to(user.room).emit("message", getTools.generateMessage(`${user.username} has joined!`, "System"));
+
+        const users = getTools.getUserByRoom(roomData.room);
+        io.to(user.room).emit("sidebarData", users);
+        callback();
+    })
+
+    socket.on("message", (message, callback) =>{
+        const user = getTools.getUserById(socket.id);
+        if(user.error){
+            return callback(user.error);
+        }
+        // const badWordsStatus = new badWords();
+        // if(badWordsStatus.isProfane(message)){
+        //     return callback("Your message contains inappropriate words!");
+        // }
+
+        io.to(user.room).emit("message", getTools.generateMessage(message, user.username));
+        callback();
+    });
+
+    socket.on("location", (location, callback) =>{
+        const user = getTools.getUserById(socket.id);
+        if(user.error){
+            return callback(user.error);
+        }
+        
+        const url = `https://www.google.com/maps?q=${location.latitude},${location.longitude}`
+        
+        io.to(user.room).emit("message", getTools.generateMessage(url, user.username));
+        callback();
+    });
+
+    socket.on("disconnect", () =>{
+        const user = getTools.getUserById(socket.id);
+        getTools.removeUser(user.username, user.room);
+        io.to(user.room).emit("message", getTools.generateMessage(`${user.username} has left!`, "System"));
+
+        const users = getTools.getUserByRoom(user.room);
+        socket.broadcast.to(user.room).emit("sidebarData", users);
+    })
+});
+
+server.listen(3000, () =>{
+    console.log("server is running");
+})
---
+++ utilis/getTools.js
@@ -0,0 +1,84 @@
+const fs = require("fs");
+
+const generateMessage = (message, username) =>{
+    return {
+        message: message,
+        username: username,
+        time: new Date().toLocaleTimeString(),
+        date: new Date().toLocaleString()
+    }
+
+}
+
+const addUser = (name, room, id) =>{
+    const file = loadFile();
+    const usernameExistence = file.find((user) =>{
+        return user.username === name && user.room == room
+    });
+    if(usernameExistence){
+        return {error: "Username already in use!"}
+    }
+
+    const user = {
+        id: id,
+        username: name,
+        room: room,
+        time: new Date().toLocaleTimeString()
+    }
+    file.push(user);
+    saveFile(file);
+    return user;
+}
+
+const getUserById = (id) =>{
+    const file = loadFile();
+    const user = file.find((user) =>{
+        return user.id === id;
+    })
+    if(!user){
+        return {error: "User not found!"}
+    }
+    return user;
+}
+
+const getUserByRoom = (room) =>{
+    const file = loadFile();
+    const users = file.filter((user) =>{
+        return user.room == room;
+    })
+    if(!users){
+        return {error: "User not found!"}
+    }
+    return users;
+}
+
+const removeUser = (name, room) =>{
+    let file = loadFile();
+    file.find((user, index) =>{
+        if(user.username === name && user.room == room){
+            return file.splice(index, 1);
+        }
+    })
+    saveFile(file);
+}
+
+const loadFile = () =>{
+    const bufferFile = fs.readFileSync("./utilis/users.json");
+    const stringFile = bufferFile.toString();
+    const file = JSON.parse(stringFile);
+
+    return file;
+}
+
+const saveFile = (file) =>{
+    const jsonFile = JSON.stringify(file);
+    fs.writeFileSync("./utilis/users.json", jsonFile);
+}
+
+module.exports = {
+    generateMessage: generateMessage,
+    addUser: addUser,
+    getUserById: getUserById,
+    getUserByRoom: getUserByRoom,
+    removeUser: removeUser
+};
